00014017ca03c03fe0de65b9805e1be2c11cb2aa
aheifetz <aheifetz@mit.edu>
2017-04-11 13:05:44 -0400

    Reorder test methods for clarity

--- main.c
+++ main.c
@@ -12,30 +12,6 @@ void binaryPrint(uint64_t out, uint8_t len) {
   }
 }
 
-void Raw_Values_Test(void) {
-  Can_FrontCanNode_RawValues_T begin;
-  begin.accel_1_raw = 100;
-  begin.accel_2_raw = 200;
-  begin.brake_1_raw = 300;
-  begin.brake_2_raw = 400;
-  begin.steering_raw = 500;
-
-  Frame mid;
-  Can_FrontCanNode_RawValues_ToCan(&begin, &mid);
-  uint64_t end_bitstring = 0;
-  to_bitstring(mid.data, &end_bitstring);
-
-  Can_FrontCanNode_RawValues_T end;
-  Can_FrontCanNode_RawValues_FromCan(&mid, &end);
-  bool equal = (
-      begin.accel_1_raw == end.accel_1_raw &&
-      begin.accel_2_raw == end.accel_2_raw &&
-      begin.brake_1_raw == end.brake_1_raw &&
-      begin.brake_2_raw == end.brake_2_raw &&
-      begin.steering_raw == end.steering_raw);
-  printf("%s \n", equal ? "RAW_VALUES_PASS" : "RAW_VALUES_FAIL");
-}
-
 void Driver_Output_Test(void) {
   Can_FrontCanNode_DriverOutput_T begin;
   begin.torque = -10000;
@@ -58,6 +34,30 @@ void Driver_Output_Test(void) {
       begin.throttle_implausible == end.throttle_implausible &&
       begin.brake_throttle_conflict == end.brake_throttle_conflict);
   printf("%s \n", equal ? "DRIVER_OUTPUT_PASS" : "DRIVER_OUTPUT_FAIL");
+}
+
+void Raw_Values_Test(void) {
+  Can_FrontCanNode_RawValues_T begin;
+  begin.accel_1_raw = 100;
+  begin.accel_2_raw = 200;
+  begin.brake_1_raw = 300;
+  begin.brake_2_raw = 400;
+  begin.steering_raw = 500;
+
+  Frame mid;
+  Can_FrontCanNode_RawValues_ToCan(&begin, &mid);
+  uint64_t end_bitstring = 0;
+  to_bitstring(mid.data, &end_bitstring);
+
+  Can_FrontCanNode_RawValues_T end;
+  Can_FrontCanNode_RawValues_FromCan(&mid, &end);
+  bool equal = (
+      begin.accel_1_raw == end.accel_1_raw &&
+      begin.accel_2_raw == end.accel_2_raw &&
+      begin.brake_1_raw == end.brake_1_raw &&
+      begin.brake_2_raw == end.brake_2_raw &&
+      begin.steering_raw == end.steering_raw);
+  printf("%s \n", equal ? "RAW_VALUES_PASS" : "RAW_VALUES_FAIL");
 }
 
 int main(void) {
