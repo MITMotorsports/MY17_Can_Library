5e1d1464591de49f3444958b49be6c0c242b3144
alec-heif <alec.heif@gmail.com>
2017-04-22 19:40:43 -0400

    Revert "TEMP hacky workaround until EVT solves CAN problem"
    This reverts commit 68155ecde31d683e5ab5a92a6f6dc3f1f4fa58e1.

--- arm_src/arm_can_drivers.c
+++ arm_src/arm_can_drivers.c
@@ -3,15 +3,11 @@
 #include <stdint.h>
 
 #include "chip.h"
-
-#include "hardware_can.c"
+#include "can.h"
+#include "ccand_11xx.h"
 
 void Can_Init(uint32_t baudrate) {
-  Hardware_CAN_Init(baudrate);
-}
-
-void Can_Pass() {
-  Hardware_CAN_Pass();
+  CAN_Init(baudrate);
 }
 
 void Can_RawWrite(Frame *frame) {
@@ -24,16 +20,16 @@ void Can_RawWrite(Frame *frame) {
     data[i] = frame->data[i];
   }
 
-  uint32_t ret = Hardware_CAN_Transmit(can_out_id, data, can_out_bytes);
-  if (ret != HARDWARE_NO_CAN_ERROR) {
+  uint32_t ret = CAN_Transmit(can_out_id, data, can_out_bytes);
+  if (ret != NO_CAN_ERROR) {
     // TODO handle error
   }
 }
 
 bool Can_RawRead(Frame *frame) {
   CCAN_MSG_OBJ_T rx_msg;
-  HARDWARE_CAN_ERROR_T ret = Hardware_CAN_Receive(&rx_msg);
-  if (ret == HARDWARE_NO_CAN_ERROR) {
+  CAN_ERROR_T ret = CAN_Receive(&rx_msg);
+  if (ret == NO_CAN_ERROR) {
     frame->id = rx_msg.mode_id;
     frame->len = rx_msg.dlc;
     uint8_t i;
@@ -41,7 +37,7 @@ bool Can_RawRead(Frame *frame) {
       frame->data[i] = rx_msg.data[i];
     }
     return true;
-  } else if (ret != HARDWARE_NO_RX_CAN_MESSAGE) {
+  } else if (ret != NO_RX_CAN_MESSAGE) {
     // TODO handle error
     return false;
   } else {
