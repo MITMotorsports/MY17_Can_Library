2e23ab7291853fc3164444525d6b36cd5790806a
Dani White <white.dani.m@gmail.com>
2017-11-04 16:30:48 -0400

    Fix Can_MC_RegID_T

--- can_validator/ids.py
+++ can_validator/ids.py
@@ -8,23 +8,43 @@ output_path = "../ids.h"
 
 spec = CANSpec(spec_path)
 
+# The VCU_MC_MESSAGE and MC_RESPONSE messages need to be combined into one enum, which will be stored here
+can_mc_reg_lines = []
+
 with open(output_path, 'w') as f:
-    f.write('_MY17_CAN_LIBRARY_IDS_H\n')
-    f.write('_MY17_CAN_LIBRARY_IDS_H\n\n')
+    f.write('#ifndef _MY17_CAN_LIBRARY_IDS_H\n')
+    f.write('#define _MY17_CAN_LIBRARY_IDS_H\n\n')
     f.write('#include "can_validator/fsae_can_spec.h"\n\n')
 
     for message in spec.messages.values():
         for segment_name, segment in message.segments.items():
             if segment.c_type == 'enum':
-                f.write('typedef enum {\n')
-                for value_name, value in segment.values.items():
-                    message_name = common.get_msg_enum_name(message.name).upper()
+                if not (message.name == "VCU_MC_MESSAGE" or message.name == "MC_RESPONSE"):
+                    f.write('typedef enum {\n')
+                    for value_name, value in segment.values.items():
+                        message_name = common.get_msg_enum_name(message.name).upper()
 
-                    # Fix name mismatch
-                    message_name = message_name.replace('HEARTBEAT', 'STATE')
-                    f.write(
-                        "  " + message_name + "_" + value_name.upper() + " = " +
-                          '____' + message.name + '__' + segment_name + '__' + value_name + ",\n")
-                f.write("} " + common.get_msg_enum_name(message.name).replace('Heartbeat', 'State') + "ID_T;\n\n")
+                        # Fix name mismatch
+                        message_name = message_name.replace('HEARTBEAT', 'STATE')
+                        f.write(
+                            "  " + message_name + "_" + value_name.upper() + " = " +
+                              '____' + message.name + '__' + segment_name + '__' + value_name + ",\n")
+                    f.write("} " + common.get_msg_enum_name(message.name).replace('Heartbeat', 'State') + "ID_T;\n\n")
+                else:
+                    for value_name, value in segment.values.items():
+                        if value_name.upper() == "TORQUE_CMD" and message.name == "VCU_MC_MESSAGE":
+                            # Both of the message have the same value name, but we only want the one from VCU_MC_MESSAGE
+                            continue
+                        val_name = value_name.upper()
+
+                        # Fix name mismatch
+                        val_name = value_name.replace("ACTUAL_FILTERED", "AFTER_DISPLAY")
+                        can_mc_reg_lines.append("  CAN_MC_REG_" + val_name + " = " + '____' + message.name + '__' +
+                            segment_name + '__' + value_name + ",\n")
+    f.write('typedef enum {\n')
+    for line in can_mc_reg_lines:
+        f.write(line)
+    f.write("} Can_MC_RegID_T;\n\n")
+
 
     f.write('#endif // _MY17_CAN_LIBRARY_IDS_H')
