1b2afb921f9c4619cab9e0d246fa6a70f61b4d7e
Dani White <white.dani.m@gmail.com>
2017-11-07 19:12:08 -0500

    Add structs to main.py

--- can_validator/main.py
+++ can_validator/main.py
@@ -1,9 +1,18 @@
 import fsae_can_spec
 import MY17_Can_Library
 import ids
+import structs
 
 spec_path = "ParseCAN/fsae_can_spec.yml"
 
 fsae_can_spec.write("fsae_can_spec.h", spec_path)
 MY17_Can_Library.write("../MY17_Can_Library.c", spec_path, "../MY17_Can_Library_BASE.txt", "special_cases.txt")
 ids.write("../ids.h", spec_path)
+structs_paths = {
+    "bms": "../bms.h",
+    "can_node": "../can_node.h",
+    "current_sensor": "../current_sensor.h",
+    "dash": "../dash.h",
+    "vcu": "../vcu.h"
+}
+structs.write(structs_paths, "ParseCAN/fsae_can_spec.yml")
