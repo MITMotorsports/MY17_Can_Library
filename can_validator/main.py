32cec91ad21ce1fde0801478b16a27359d902ba9
Dani White <white.dani.m@gmail.com>
2017-11-11 14:49:08 -0500

    Generate MY17_Can_Library.h

--- can_validator/main.py
+++ can_validator/main.py
@@ -4,10 +4,20 @@ import ids
 import structs
 
 spec_path = "ParseCAN/fsae_can_spec.yml"
+header_spec_path =
 
-fsae_can_spec.write("fsae_can_spec.h", spec_path)
-MY17_Can_Library.write("../MY17_Can_Library.c", spec_path, "../MY17_Can_Library_BASE.txt", "special_cases.txt")
-ids.write("../ids.h", spec_path)
+unused_messages = [
+    "Lv_Battery_Voltage",
+    "Accelerometer_Horizontal",
+    "Accelerometer_Vertical",
+    "Gyro_Vertical",
+    "Gyro_Horizontal",
+    "Magnetometer_Horizontal",
+    "Magnetometer_Vertical",
+    "Mc_Response",
+    "Vcu_Mc_Message"
+]
+
 structs_paths = {
     "bms": "../bms.h",
     "cannode": "../can_node.h",
@@ -15,4 +25,8 @@ structs_paths = {
     "dash": "../dash.h",
     "vcu": "../vcu.h"
 }
+
+fsae_can_spec.write("fsae_can_spec.h", spec_path)
+MY17_Can_Library.write("../MY17_Can_Library.c", spec_path, "../MY17_Can_Library_BASE.txt", "special_cases.txt")
+ids.write("../ids.h", spec_path)
 structs.write(structs_paths, "ParseCAN/fsae_can_spec.yml")
